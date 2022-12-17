1798be799cff7996f766846e3b6bffa39a7f6d48
rhx1138 <ryanjhendrix@gmail.com>
2022-12-17 11:24:30 -0500

    updated readme with website, updated script to change bg img with the time of day

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # time-of-day
 A time of day app
+
+
+[Time of day App Website](https://time-of-day-app.netlify.app/)
--- script.js
+++ script.js
@@ -2,3 +2,15 @@ setInterval(function(){
     var date=new Date();
     document.getElementById("clock").innerHTML=date.toLocaleTimeString();
     },1000);
+
+    setInterval(function(){
+        var date=new Date();
+        var hour=date.getHours();
+        var img;
+        if(hour>=7&&hour<12)img="morning.jpg";
+        else if(hour>=12&&hour<17)img="noon.jpg";
+        else if(hour>=17&&hour<19)img="evening.jpg";
+        else img="night.jpg";
+        document.body.style.backgroundImage="url("+img+")";
+      },1000);
+    
