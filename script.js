d04fe62770850d9d3d7b8c94ed8159c1e07d9232
rhx1138 <ryanjhendrix@gmail.com>
2022-12-17 11:39:28 -0500

    optimized img for faster page loading

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ setInterval(function(){
         var date=new Date();
         var hour=date.getHours();
         var img;
-        if(hour>=7&&hour<12)img="morning.jpg";
-        else if(hour>=12&&hour<17)img="noon.jpg";
-        else if(hour>=17&&hour<19)img="dusk.jpg";
-        else img="night.jpg";
+        if(hour>=7&&hour<12)img="morning1.jpg";
+        else if(hour>=12&&hour<17)img="noon1.jpg";
+        else if(hour>=17&&hour<19)img="dusk1.jpg";
+        else img="night1.jpg";
         document.body.style.backgroundImage="url("+img+")";
       },1000);
     
