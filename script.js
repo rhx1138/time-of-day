14ba6045ae586dab010d9d35b0305fa61c0a8148
rhx1138 <ryanjhendrix@gmail.com>
2022-12-17 11:49:53 -0500

    updated file directories, images

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ setInterval(function(){
         var date=new Date();
         var hour=date.getHours();
         var img;
-        if(hour>=7&&hour<12)img="morning1.jpg";
-        else if(hour>=12&&hour<17)img="noon1.jpg";
-        else if(hour>=17&&hour<19)img="dusk1.jpg";
-        else img="night1.jpg";
+        if(hour>=7&&hour<12)img="./images/morning1.jpg";
+        else if(hour>=12&&hour<17)img="./images/noon1.jpg";
+        else if(hour>=17&&hour<19)img="./images/dusk1.jpg";
+        else img="./images/night1.jpg";
         document.body.style.backgroundImage="url("+img+")";
       },1000);
     
+
+     // ./images/morning1.jpg
